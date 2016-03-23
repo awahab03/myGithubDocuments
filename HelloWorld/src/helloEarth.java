97ff77c16ee89096473039090addb9b36baa9e1b
awahab03 <awahab03@yahoo.com>
2016-03-23 15:20:20 -0400

    added the demo project
    a test

---
+++ HelloWorld/src/helloEarth.java
@@ -0,0 +1,10 @@
+
+public class helloEarth {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("Hello Earth, how are your people doing?");
+
+	}
+
+}
